79d5f3cc7618773338b0fd37116a8674ba26863d
Lorenzo Pisani <zeelot3k@gmail.com>
2014-12-11 16:55:26 -0800

    implement the strategy interface with the dummy strategy

--- game/strategy_dummy.go
+++ game/strategy_dummy.go
@@ -2,7 +2,11 @@ package game
 
 type DummyStrategy struct{}
 
+func (strategy *DummyStrategy) Reset() {
+	// Nothing to reset.
+}
+
 // The dummy strategy actually works by keeping all the dice. Always.
-func (strategy DummyStrategy) ChooseDice(roll []int) []int {
+func (strategy *DummyStrategy) ChooseDice(roll []int) []int {
 	return roll
 }
